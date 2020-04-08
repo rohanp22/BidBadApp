5e38ed2cdf112f1cabf1f17c010006ac15e9c875
Rohan <rohanpandey2210@gmail.com>
2020-04-08 22:03:56 +0530

    optimized home

--- app/src/main/java/com/wielabs/Fragments/HomeFragment.java
+++ app/src/main/java/com/wielabs/Fragments/HomeFragment.java
@@ -65,30 +65,21 @@ import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 
-public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener, ListenerImage {
+public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
 
-    public ArrayList<Current_Product> current_products = new ArrayList<>();
-    public List<CartItems> cartItems = new ArrayList<>();
-    Dialog dialog;
-    Button mButton;
-    int bal;
-    ProgressBar p;
-    View view1;
-    View view;
-    Context c;
+    private ArrayList<Current_Product> current_products = new ArrayList<>();
+    private Dialog dialog;
+    private ProgressBar p;
+    private View view;
+    private Context c;
     public RecyclerViewAdapterCurrent adapter;
-    SwipeRefreshLayout mSwipeRefreshLayout;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setHasOptionsMenu(true);
-        p = getActivity().findViewById(R.id.progressBarHome);
-        c = getActivity();
-        dialog = new Dialog(getContext());
-        dialog.setContentView(R.layout.bid_dialog);
-        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
     }
 
 
@@ -106,23 +97,26 @@ public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefre
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_home, container, false);
-        //this.view = view;
-        //Home.navigation.setSelectedItemId(R.id.navigation_home);
-        view1 = view;
+
+        p = view.findViewById(R.id.progressBarHome);
+        c = view.getContext();
+        dialog = new Dialog(view.getContext());
+        dialog.setContentView(R.layout.bid_dialog);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
         loadCurrentProducts(view);
         return view;
     }
 
     @Override
     public void onViewCreated(final @NonNull View view, @Nullable Bundle savedInstanceState) {
-        //this.view = view;
-        p = view.findViewById(R.id.progressBarHome);
-        c = view.getContext();
 
-        dialog = new Dialog(view.getContext());
-        dialog.setContentView(R.layout.bid_dialog);
-        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        ImageView walletimg = (ImageView) view.findViewById(R.id.walletimg);
+        ImageView walletimg = view.findViewById(R.id.walletimg);
+        RecyclerView sliderView = view.findViewById(R.id.sliderview2);
+        sliderView.setLayoutManager(new LinearLayoutManager(view.getContext(), RecyclerView.HORIZONTAL, false));
+        SliderAdapter adapter = new SliderAdapter(view.getContext(), sliderView);
+        sliderView.setAdapter(adapter);
+
         walletimg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -133,16 +127,11 @@ public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefre
             }
         });
 
-        mSwipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.refresh);
+        mSwipeRefreshLayout = view.findViewById(R.id.refresh);
         mSwipeRefreshLayout.setOnRefreshListener(this);
-
-        RecyclerView sliderView = view.findViewById(R.id.sliderview2);
-        sliderView.setLayoutManager(new LinearLayoutManager(view.getContext(), RecyclerView.HORIZONTAL, false));
-        SliderAdapter adapter = new SliderAdapter(view.getContext(), sliderView);
-        sliderView.setAdapter(adapter);
     }
 
-    void loadCurrentProducts(final View view) {
+    private void loadCurrentProducts(final View view) {
 
         StringRequest stringRequest = new StringRequest(Request.Method.GET, "http://easyvela.esy.es/AndroidAPI/currentproducts.php?id="+SharedPrefManager.getInstance(getContext()).getUser().getId(),
                 new Response.Listener<String>() {
@@ -188,16 +177,13 @@ public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefre
         requestQueue.add(stringRequest);
     }
 
-    public RecyclerView recyclerViewCurrent;
-
     private void initRecyclerView(final View view) {
-
-        //LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity(), RecyclerView.VERTICAL, false);
-
         StaggeredGridLayoutManager gridLayoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL );
-        recyclerViewCurrent = (RecyclerView) view.findViewById(R.id.recyclerviewCurrent);
+        RecyclerView recyclerViewCurrent = view.findViewById(R.id.recyclerviewCurrent);
+
         if(current_products.size() == 0)
             recyclerViewCurrent.setVisibility(View.GONE);
+
         Drawable verticalDivider = ContextCompat.getDrawable(view.getContext(), R.drawable.vertical_divider);
         DividerItemDecoration verticalDecoration = new DividerItemDecoration(recyclerViewCurrent.getContext(),
                 DividerItemDecoration.HORIZONTAL);
@@ -206,10 +192,10 @@ public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefre
         DividerItemDecoration horizontalDecoration = new DividerItemDecoration(recyclerViewCurrent.getContext(),
                 DividerItemDecoration.VERTICAL);
         horizontalDecoration.setDrawable(horizontalDivider);
-//        recyclerViewCurrent.addItemDecoration(verticalDecoration);
-//        recyclerViewCurrent.addItemDecoration(horizontalDecoration);
+
         recyclerViewCurrent.setLayoutManager(gridLayoutManager);
-        adapter = new RecyclerViewAdapterCurrent(view.getContext(), current_products, this);
+        adapter = new RecyclerViewAdapterCurrent(view.getContext(), current_products);
+
         view.findViewById(R.id.progressBarHome).setVisibility(View.GONE);
         recyclerViewCurrent.setAdapter(null);
         recyclerViewCurrent.setAdapter(adapter);
@@ -221,50 +207,17 @@ public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefre
         loadCurrentProducts(view);
     }
 
-    @Override
-    public void onKittenClicked(RecyclerViewAdapterCurrent.ViewHolder holder, int position) {
-        ProductDescription kittenDetails = new ProductDescription();
-
-        Bundle args = new Bundle();
-        args.putString("YourKey", current_products.get(position).getId());
-        args.putString("show","yes");
-        args.putString("im", current_products.get(position).getImage_url());
-        kittenDetails.setArguments(args);
-
-        // Note that we need the API version check here because the actual transition classes (e.g. Fade)
-        // are not in the support library and are only available in API 21+. The methods we are calling on the Fragment
-        // ARE available in the support library (though they don't do anything on API < 21)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            kittenDetails.setSharedElementEnterTransition(new DetailsTransition());
-            kittenDetails.setEnterTransition(new Fade());
-            setExitTransition(new Fade());
-            kittenDetails.setSharedElementReturnTransition(new DetailsTransition());
-        }
-
-        getActivity().getSupportFragmentManager()
-                .beginTransaction()
-                .addSharedElement(holder.image, "trans")
-                .replace(R.id.fragment_container, kittenDetails)
-                .addToBackStack(null)
-                .commit();
-    }
-
     public class RecyclerViewAdapterCurrent extends RecyclerView.Adapter<RecyclerViewAdapterCurrent.ViewHolder> {
 
-        int id;
         int position;
-        private static final String TAG = "RecyclerViewAdapterCurrent";
-        long diff;
         Date startDate1;
         String date;
         ArrayList<Current_Product> current_products;
         private Context mContext;
-        ListenerImage mlistener;
 
-        public RecyclerViewAdapterCurrent(Context context, ArrayList<Current_Product> current_product, ListenerImage mlistener) {
+        public RecyclerViewAdapterCurrent(Context context, ArrayList<Current_Product> current_product) {
             this.current_products = current_product;
             mContext = context;
-            this.mlistener = mlistener;
         }
 
         @Override
@@ -273,8 +226,6 @@ public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefre
             return new ViewHolder(view);
         }
 
-        int bal;
-
         @Override
         public void onBindViewHolder(final RecyclerViewAdapterCurrent.ViewHolder holder,final int position) {
 
@@ -282,8 +233,6 @@ public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefre
             this.position = position;
             holder.setIsRecyclable(false);
             date = current_products.get(position).getEnd_date();
-            //loadCurrentProducts(view);
-
 
             SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss a");
 
@@ -299,13 +248,9 @@ public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefre
                     .load(current_products.get(position).getImage_url())
                     .into(holder.image);
 
-            //holder.image.setImageDrawable(getResources().getDrawable(R.drawable.placekitten_1));
-
             holder.name.setText(current_products.get(position).getTitle());
 
             ViewCompat.setTransitionName(holder.image, String.valueOf(position) + "_image");
-
-            //holder.mrp.setText(current_products.get(position).getMrp());
 
             holder.sp.setText("Entry : ₹"+current_products.get(position).getSp());
 
@@ -344,7 +289,8 @@ public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefre
 
                         final long elapsedSeconds = different / secondsInMilli;
 
-                        String curtime = elapsedHours + "hr " + String.format("%02d", elapsedMinutes) + "m " + String.format("%02d", elapsedSeconds) + "s";
+                        String curtime;
+
                         if(elapsedHours > 0) {
                             curtime = elapsedHours + "hr " + String.format("%02d", elapsedMinutes) + "m";
                             holder.time.setText(curtime);
@@ -361,14 +307,22 @@ public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefre
                 }
 
                 public void onFinish() {
-                    getFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+                    getFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragment_container, new HomeFragment())
+                            .commit();
                 }
+
             }.start();
 
             holder.bid.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    showBottomSheet(current_products.get(position).getId(), current_products.get(position).getImage_url(), current_products.get(position).getTitle(), current_products.get(position).getSp());
+                    showBottomSheet(current_products.get(position).getId(),
+                            current_products.get(position).getImage_url(),
+                            current_products.get(position).getTitle(),
+                            current_products.get(position).getSp()
+                    );
                 }
             });
 
@@ -379,12 +333,6 @@ public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefre
                     i.putExtra("imageurl", current_products.get(position).getImage_url());
                     i.putExtra("YourKey", current_products.get(position).getId());
                     startActivity(i);
-//                    ProductDescription ldf = new ProductDescription ();
-//                    Bundle args = new Bundle();
-//                    args.putString("YourKey", current_products.get(position).getId());
-//                    args.putString("show","yes");
-//                    ldf.setArguments(args);
-//                    mlistener.onKittenClicked(holder, position);
                 }
             });
         }
@@ -395,10 +343,6 @@ public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefre
                     ActionBottomDialogFragment.newInstance(productId, imageurl, titleString, sp);
             addPhotoBottomDialogFragment.show(getFragmentManager(),
                     ActionBottomDialogFragment.TAG);
-        }
-
-        public boolean updateBalance() {
-            return true;
         }
 
         @Override
@@ -436,8 +380,6 @@ public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefre
 
     class sortTime implements Comparator<Current_Product>
     {
-        // Used for sorting in ascending order of
-        // roll number
         public int compare(Current_Product a, Current_Product b)
         {
             SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss a");
@@ -472,7 +414,6 @@ public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefre
             ViewGroup.LayoutParams params = view.getLayoutParams();
             params.width = (int)(width * 0.7);
             view.setLayoutParams(params);
-
             return new SliderAdapter.ViewHolder(view);
         }
 
@@ -500,7 +441,6 @@ public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefre
                             .load(url)
                             .into(holder.image);
                     break;
-
             }
         }
 
@@ -510,12 +450,9 @@ public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefre
         }
 
         public class ViewHolder extends RecyclerView.ViewHolder {
-
             ImageView image;
-
             public ViewHolder(@NonNull View itemView) {
                 super(itemView);
-                //listView = itemView.findViewById(R.id.bidHistory);
                 image = itemView.findViewById(R.id.image_view);
 
             }
