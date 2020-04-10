900d47f00933464d59c42bef2fd76094bdb22468
Rohan <rohanpandey2210@gmail.com>
2020-04-10 17:11:27 +0530

    homefragment

---
+++ app/src/main/java/com/wielabs/Fragments/EditFragment.java
@@ -0,0 +1,35 @@
+package com.wielabs.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.wielabs.R;
+
+public class EditFragment extends Fragment {
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.edit_layout, container, false);
+
+        return view;
+    }
+
+}
--- app/src/main/java/com/wielabs/Fragments/ProfileFragment.java
+++ app/src/main/java/com/wielabs/Fragments/ProfileFragment.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.widget.ImageView;
 
 import com.wielabs.R;
 
@@ -29,7 +30,20 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        ImageView profileEdit = view.findViewById(R.id.profileEdit);
+        profileEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment_container,new EditFragment())
+                        .commit();
+            }
+        });
+
+        return view;
     }
 
 }
