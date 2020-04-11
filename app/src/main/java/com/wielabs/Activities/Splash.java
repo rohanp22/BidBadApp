da8549c42d6361d8f4444a55136f69a0c694c389
Rohan Panday <30382730+rohanp22@users.noreply.github.com>
2020-04-11 10:36:20 +0530

    Splash

--- app/src/main/java/com/wielabs/Activities/Splash.java
+++ app/src/main/java/com/wielabs/Activities/Splash.java
@@ -15,10 +15,6 @@ public class Splash extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Window window = Splash.this.getWindow();
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        window.setStatusBarColor(ContextCompat.getColor(Splash.this, R.color.colorPrimary));
 
         if(SharedPrefManager.getInstance(this).isLoggedIn()){
             startActivity(new Intent(Splash.this, Home.class));
