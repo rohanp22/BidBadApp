0ead9c67301c88d0a6ceedc06e42b1664895ec63
Rohan Panday <30382730+rohanp22@users.noreply.github.com>
2020-04-11 10:35:21 +0530

    Splash

--- app/src/main/java/com/wielabs/Activities/Splash.java
+++ app/src/main/java/com/wielabs/Activities/Splash.java
@@ -1,10 +1,10 @@
 package com.wielabs.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
 import com.wielabs.Others.SharedPrefManager;
@@ -12,35 +12,19 @@ import com.wielabs.R;
 
 public class Splash extends AppCompatActivity {
 
-    private static int SPLASH_TIME_OUT = 1000;
-    Intent i;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_splash);
+        Window window = Splash.this.getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(ContextCompat.getColor(Splash.this, R.color.colorPrimary));
 
         if(SharedPrefManager.getInstance(this).isLoggedIn()){
-            i = new Intent(Splash.this, Home.class);
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    startActivity(i);
-                    finish();
-                }
-            }, SPLASH_TIME_OUT);
+            startActivity(new Intent(Splash.this, Home.class));
         }
         else {
-            i = new Intent(Splash.this, Login.class);
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    startActivity(i);
-                    finish();
-                }
-            }, SPLASH_TIME_OUT);
+            startActivity(new Intent(Splash.this, Login.class));
         }
 
     }
