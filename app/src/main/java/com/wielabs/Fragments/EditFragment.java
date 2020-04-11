31b8aeedc33e1ddb193f823b467272899d535e74
Rohan <rohanpandey2210@gmail.com>
2020-04-11 22:39:07 +0530

    Splash

--- app/src/main/java/com/wielabs/Fragments/EditFragment.java
+++ app/src/main/java/com/wielabs/Fragments/EditFragment.java
@@ -1,10 +1,17 @@
 package com.wielabs.Fragments;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,6 +28,8 @@ import java.util.ArrayList;
 
 public class EditFragment extends Fragment {
 
+    Dialog dialog;
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -35,6 +44,7 @@ public class EditFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.edit_layout, container, false);
+        dialog = new Dialog(view.getContext());
         RecyclerView editRecycler = view.findViewById(R.id.editRecycler);
         RecyclerView editPrivate = view.findViewById(R.id.editRecycler1);
         ArrayList<Item> items = new ArrayList<>();
@@ -42,8 +52,11 @@ public class EditFragment extends Fragment {
 
         items.add(new Item("Name", "Rohan"));
         items.add(new Item("Referral code", "rohan3241"));
-        items.add(new Item("Gender", "Male"));
-        items.add(new Item("Date of birth", "01-01-1998"));
+        items.add(new Item("Address", "19-1-924/A/11/1 \nMurli nagar,\nHyderabad - 500064"));
+        items.add(new Item("Change password", ""));
+
+        items1.add(new Item("Gender", "Male"));
+        items1.add(new Item("Date of birth", "01-01-1998"));
 
         items1.add(new Item("Mobile", "+91-8309096401"));
         items1.add(new Item("E-mail", "rohanpandey2210@gmail.com"));
@@ -106,6 +119,11 @@ public class EditFragment extends Fragment {
                     @Override
                     public void onClick(View view) {
                         Toast.makeText(context, heroList.get(getAdapterPosition()).getValue(), Toast.LENGTH_SHORT).show();
+                        if(heroList.get(getAdapterPosition()).getTitle().equals("Change password")){
+                            dialog.setContentView(R.layout.change_password_dialog);
+                            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+                            dialog.show();
+                        }
                     }
                 });
             }
