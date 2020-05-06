7611c57016d75c821472273b2c9f05ad7b5a6b57
Rohan <rohanpandey2210@gmail.com>
2020-05-06 22:05:21 +0530

    Merge branch 'master' of https://github.com/rohanp22/BidBadApp
     Conflicts:
    	.idea/workspace.xml
    
    Signed-off-by: Rohan <rohanpandey2210@gmail.com>

--- app/src/main/java/com/wielabs/Fragments/RewardsFragment.java
+++ app/src/main/java/com/wielabs/Fragments/RewardsFragment.java
@@ -1,15 +1,20 @@
 package com.wielabs.Fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.anupkumarpanwar.scratchview.ScratchView;
 import com.wielabs.R;
+
+import static com.facebook.accountkit.internal.AccountKitController.getApplicationContext;
 
 public class RewardsFragment extends Fragment {
 
@@ -22,6 +27,22 @@ public class RewardsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_rewards, container, false);
-        return super.onCreateView(inflater, container, savedInstanceState);
+        ScratchView scratchView = view.findViewById(R.id.scratch_view);
+        scratchView.setRevealListener(new ScratchView.IRevealListener() {
+            @Override
+            public void onRevealed(ScratchView scratchView) {
+                Toast.makeText(getApplicationContext(), "Reveled", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onRevealPercentChangedListener(ScratchView scratchView, float percent) {
+                if (percent >= 0.5) {
+                    Log.d("Reveal Percentage", "onRevealPercentChangedListener: " + String.valueOf(percent));
+                    scratchView.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        return view;
     }
 }
